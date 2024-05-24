5317a06c998483d0669f11a34480ae3dd2f20d22
Victor Le Roy Matos <54508986+vmleroy@users.noreply.github.com>
2024-05-24 15:02:55 -0300

    Update default.ts

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,11 +1,11 @@
 export const defaultTheme = {
     colors: {
         'main-green': '#0EC281',
-        'secondary-green': '##048556',
+        'secondary-green': '#048556',
         'main-back': '#161918',
         'gray': '#F6F6F6',
         'secundary-gray': '#3F3D3D',
-        'white':'##FFFFFF',
+        'white':'#FFFFFF',
     },
     gradient: {
         'CTA': 'radial-gradient(#01B473, #70BE81, #048556)',
