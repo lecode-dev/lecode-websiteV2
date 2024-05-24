c59d528fbf4dd6fbf0658244b65d9b36b2415b81
Daniel <danielnorges@gmail.com>
2024-05-24 11:56:05 -0300

    feat: create global theme

---
+++ src/styles/global.ts
@@ -0,0 +1,90 @@
+import styled, { createGlobalStyle } from "styled-components"
+export const GlobalStyle = createGlobalStyle`
+
+* {
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+
+`
+export const Header1 = styled.h1 `
+  font-family: Montserrat, sans-serif;
+font-weight: bold;
+font-size: 48px;
+line-height: 125%;
+letter-spacing: 0;
+text-decoration: none;
+margin-top: 0;
+margin-bottom: 0;
+text-transform: none;
+`
+
+export const Subtitle = styled.h2 `
+font-family: Montserrat, sans-serif;
+font-weight: bold;
+font-size: 24px;
+line-height: 125%;
+letter-spacing: 0;
+text-decoration: none;
+margin-top: 0;
+margin-bottom: 0;
+text-transform: none;
+`
+
+export const Header32 = styled.h3 `
+  font-family: Montserrat, sans-serif;
+font-weight: bold;
+font-size: 32px;
+line-height: 125%;
+letter-spacing: 0;
+text-decoration: none;
+margin-top: 0;
+margin-bottom: 0;
+text-transform: none; 
+`
+
+export const Header24 = styled.h4 `
+  font-family: Montserrat, sans-serif;
+font-weight: bold;
+font-size: 24px;
+line-height: 125%;
+letter-spacing: 0;
+text-decoration: none;
+margin-top: 0;
+margin-bottom: 0;
+text-transform: none; 
+`
+export const subtitle2 = styled.h2 `
+  font-family: Montserrat, sans-serif;
+font-weight: bold;
+font-size: 24px;
+line-height: 125%;
+letter-spacing: 0;
+text-decoration: none;
+margin-top: 0;
+margin-bottom: 0;
+text-transform: none; 
+`
+export const paragrafo1 = styled.p `
+  font-family: Poppins, sans-serif;
+font-weight: 400;
+font-size: 16px;
+line-height: 125%;
+letter-spacing: 0;
+text-decoration: none;
+margin-top: 0;
+margin-bottom: 0;
+text-transform: none; 
+`
+export const paragrafo2 = styled.p `
+  font-family: Poppins, sans-serif;
+font-weight: 500;
+font-size: 20px;
+line-height: 125%;
+letter-spacing: 0;
+text-decoration: none;
+margin-top: 0;
+margin-bottom: 0;
+text-transform: none; 
+`
