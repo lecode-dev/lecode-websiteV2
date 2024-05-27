efd411e0a17def4d7034770cd4160e08bca37ec8
Daniel <danielnorges@gmail.com>
2024-05-27 13:32:39 -0300

    feat: responsive generic styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,90 +1,88 @@
-import styled, { createGlobalStyle } from "styled-components"
-export const GlobalStyle = createGlobalStyle`
+import styled, { createGlobalStyle, CSSProperties } from "styled-components";
 
-* {
-margin: 0;
-padding: 0;
-box-sizing: border-box;
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+interface StyledComponentProps {
+  fontFamily: string;
+  fontWeight: string | number;
+  fontSize: string;
+  lineHeight: string;
 }
 
-`
-export const Header1 = styled.h1 `
-  font-family: Montserrat, sans-serif;
-font-weight: bold;
-font-size: 48px;
-line-height: 125%;
-letter-spacing: 0;
-text-decoration: none;
-margin-top: 0;
-margin-bottom: 0;
-text-transform: none;
-`
+const createStyledHeading = (
+  tag: keyof JSX.IntrinsicElements,
+  { fontFamily, fontWeight, fontSize, lineHeight }: StyledComponentProps
+) => styled(tag)`
+  font-family: ${fontFamily}, sans-serif;
+  font-weight: ${fontWeight};
+  font-size: ${fontSize};
+  line-height: ${lineHeight};
+  letter-spacing: 0;
+  text-decoration: none;
+  margin-top: 0;
+  margin-bottom: 0;
+  text-transform: none;
+`;
 
-export const Subtitle = styled.h2 `
-font-family: Montserrat, sans-serif;
-font-weight: bold;
-font-size: 24px;
-line-height: 125%;
-letter-spacing: 0;
-text-decoration: none;
-margin-top: 0;
-margin-bottom: 0;
-text-transform: none;
-`
+const createStyledParagraph = (
+  tag: keyof JSX.IntrinsicElements,
+  { fontFamily, fontWeight, fontSize, lineHeight }: StyledComponentProps
+) => styled(tag)`
+  font-family: ${fontFamily}, sans-serif;
+  font-weight: ${fontWeight};
+  font-size: ${fontSize};
+  line-height: ${lineHeight};
+  letter-spacing: 0;
+  text-decoration: none;
+  margin-top: 0;
+  margin-bottom: 0;
+  text-transform: none;
+`;
 
-export const Header32 = styled.h3 `
-  font-family: Montserrat, sans-serif;
-font-weight: bold;
-font-size: 32px;
-line-height: 125%;
-letter-spacing: 0;
-text-decoration: none;
-margin-top: 0;
-margin-bottom: 0;
-text-transform: none; 
-`
+export const Header1 = createStyledHeading('h1', {
+  fontFamily: 'Montserrat',
+  fontWeight: 'bold',
+  fontSize: '48px',
+  lineHeight: '125%'
+});
 
-export const Header24 = styled.h4 `
-  font-family: Montserrat, sans-serif;
-font-weight: bold;
-font-size: 24px;
-line-height: 125%;
-letter-spacing: 0;
-text-decoration: none;
-margin-top: 0;
-margin-bottom: 0;
-text-transform: none; 
-`
-export const Subtitle2 = styled.h2 `
-  font-family: Montserrat, sans-serif;
-font-weight: bold;
-font-size: 24px;
-line-height: 125%;
-letter-spacing: 0;
-text-decoration: none;
-margin-top: 0;
-margin-bottom: 0;
-text-transform: none; 
-`
-export const Paragraph1 = styled.p `
-  font-family: Poppins, sans-serif;
-font-weight: 400;
-font-size: 16px;
-line-height: 125%;
-letter-spacing: 0;
-text-decoration: none;
-margin-top: 0;
-margin-bottom: 0;
-text-transform: none; 
-`
-export const Paragraph2 = styled.p `
-  font-family: Poppins, sans-serif;
-font-weight: 500;
-font-size: 20px;
-line-height: 125%;
-letter-spacing: 0;
-text-decoration: none;
-margin-top: 0;
-margin-bottom: 0;
-text-transform: none; 
-`
+export const Subtitle = createStyledHeading('h2', {
+  fontFamily: 'Montserrat',
+  fontWeight: 'bold',
+  fontSize: '24px',
+  lineHeight: '125%'
+});
+
+export const Header32 = createStyledHeading('h3', {
+  fontFamily: 'Montserrat',
+  fontWeight: 'bold',
+  fontSize: '32px',
+  lineHeight: '125%'
+});
+
+export const Header24 = createStyledHeading('h4', {
+  fontFamily: 'Montserrat',
+  fontWeight: 'bold',
+  fontSize: '24px',
+  lineHeight: '125%'
+});
+
+export const Paragraph1 = createStyledParagraph('p', {
+  fontFamily: 'Poppins',
+  fontWeight: '400',
+  fontSize: '16px',
+  lineHeight: '125%'
+});
+
+export const Paragraph2 = createStyledParagraph('p', {
+  fontFamily: 'Poppins',
+  fontWeight: '500',
+  fontSize: '20px',
+  lineHeight: '125%'
+});
