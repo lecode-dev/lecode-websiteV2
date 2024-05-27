6dbb080a805ea91a80477d694d689b7f9d19be9e
Daniel <danielnorges@gmail.com>
2024-05-27 15:33:05 -0300

    feat: remove global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
 import { ThemeProvider } from '@/components/Providers/ThemeProvider/theme-provider';
 import { ResetCSS } from '@/styles';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body>
         <ThemeProvider>
           <ResetCSS />
           {children}
