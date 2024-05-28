22e9efe71860861d376a314db17e7ba265beca08
karen myllena <karen.myllena@gmail.com>
2024-05-28 17:16:53 -0300

    change font size value from px to em

--- src/styles/typographies.ts
+++ src/styles/typographies.ts
@@ -44,41 +44,41 @@ export const createStyledParagraph = (
 export const Header1 = createStyledHeading('h1', {
   fontFamily: 'Montserrat',
   fontWeight: fonts.bold,
-  fontSize: '48px',
+  fontSize: '3em',
   lineHeight: '125%'
 });
 
 export const Subtitle = createStyledHeading('h2', {
   fontFamily: 'Montserrat',
   fontWeight: fonts.bold,
-  fontSize: '24px',
+  fontSize: '1.5em',
   lineHeight: '125%'
 });
 
 export const Header32 = createStyledHeading('h3', {
   fontFamily: 'Montserrat',
   fontWeight: fonts.bold,
-  fontSize: '32px',
+  fontSize: '2em',
   lineHeight: '125%'
 });
 
 export const Header24 = createStyledHeading('h4', {
   fontFamily: 'Montserrat',
   fontWeight: fonts.bold,
-  fontSize: '24px',
+  fontSize: '1.5em',
   lineHeight: '125%'
 });
 
 export const Paragraph1 = createStyledParagraph('p', {
   fontFamily: 'Poppins',
   fontWeight: fonts.normal,
-  fontSize: '16px',
+  fontSize: '1em',
   lineHeight: '125%'
 });
 
 export const Paragraph2 = createStyledParagraph('p', {
   fontFamily: 'Poppins',
   fontWeight: fonts.medium,
-  fontSize: '20px',
+  fontSize: '1.25em',
   lineHeight: '125%'
 });
