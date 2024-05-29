6b63fa6ffd16e1a8886921fbec3303d267737c19
karen myllena <karen.myllena@gmail.com>
2024-05-29 15:11:38 -0300

    scroll bar change design

--- src/app/(components)/(body)/body.tsx
+++ src/app/(components)/(body)/body.tsx
@@ -3,4 +3,37 @@ import styled from "styled-components";
 
 export const Body = styled.div`
   margin-top: 7rem;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
+
+export const ScrollbarWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  max-height: 900px;
+  overflow-y: scroll;
+  scrollbar-color: ${({ theme }) => theme.colors.gray};
+;
+  direction: ltr; // Show the scrollbar on the right: ;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px grey;
+    border-radius: 5px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background:  ${({ theme }) => theme.colors.black};
+;
+    border-radius: 15px;
+    height: 2px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: ${({ theme }) => theme.colors.mainGreen};
+    max-height: 10px;
+  }
 `;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 import { Navbar } from '@/components/Navbar';
 import { TestimonyContainer } from './(components)/(testimony)/testimony-container';
 import { TestimonyData } from './(data)/(mock)/testimonies';
-import { Body } from './(components)/(body)/body';
+import { Body, ScrollbarWrapper } from './(components)/(body)/body';
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      <Navbar/>
+      <ScrollbarWrapper>
       <Body>
         <TestimonyContainer
           id='clients'
           testimonies={TestimonyData}
         />
       </Body>
+      </ScrollbarWrapper>
     </main>
   );
 }
