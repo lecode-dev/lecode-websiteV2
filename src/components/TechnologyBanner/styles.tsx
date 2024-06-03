24ca7a59c686d04ae301eaf8140b14e3cfd10725
Daniel <danielnorges@gmail.com>
2024-06-03 14:02:44 -0300

    feat: new description technologies

--- src/components/TechnologyBanner/styles.tsx
+++ src/components/TechnologyBanner/styles.tsx
@@ -1,8 +1,8 @@
-import { Header1, Subtitle,Header32 } from '@/styles';
+import { Header1, Subtitle, Header32, Paragraph1 } from '@/styles';
 import styled from 'styled-components';
 
 export const TechnologyContainer = styled.div`
-  height: 878px;
+  height: 978px;
   padding-top: 10rem;
   display: flex;
   flex-direction: column;
@@ -13,7 +13,7 @@ export const TechnologyContainer = styled.div`
 export const ContainerMainText = styled.div`
   width: 850px;
   height: 200px;
- margin-bottom: 84px;
+  margin-bottom: 48px;
 `;
 interface LearnAboutTextProps {
   color?: string;
@@ -27,24 +27,66 @@ export const TextRow = styled.span`
 
 export const LearnAboutText = styled(Header1)<LearnAboutTextProps>`
   text-align: center;
-  padding-right:1rem;
+  padding-right: 1rem;
   color: ${({ color, theme }) => (color === 'green' ? theme.colors.mainGreen : theme.colors.white)};
-  text-shadow: 0px 0px 5px ${({ theme }) => theme.colors.mainGreen}; 
+  text-shadow: 0px 0px 5px ${({ theme }) => theme.colors.mainGreen};
 `;
 export const ContainerSubText = styled.div`
   border: 5px solid purple;
-  margin-top:1rem;
- 
+  margin-top: 1rem;
 `;
 export const MoreTechnologiesText = styled(Subtitle)`
-color: ${({ theme }) => theme.colors.white};
-text-align: center;
-padding-top: 0.5rem;
-text-shadow: 0px 0px 5px ${({ theme }) => theme.colors.mainGreen}; 
+  color: ${({ theme }) => theme.colors.white};
+  text-align: center;
+  padding-top: 0.5rem;
+  text-shadow: 0px 0px 5px ${({ theme }) => theme.colors.mainGreen};
 `;
 
 export const ContainerTechnology = styled.div`
-  width: 1440px;
-  height: 540px;
-  border: 5px solid red;
+  width: 100%;
+  height: 900px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const CardTechnology = styled.div`
+  height: 100%;
+`;
+
+export const TitleCardTechnology = styled(Header32)`
+  color: ${({ theme }) => theme.colors.white};
+  width: 209px;
+  margin-bottom: 18px;
+`;
+export const MainTitleTechnology = styled(Subtitle)`
+  color: ${({ theme }) => theme.colors.white};
+  width: 209px;
+  margin-bottom: 18px;
+
+`;
+
+export const TechnologyDescription = styled(Paragraph1)`
+  color: ${({ theme }) => theme.colors.white};
+  width: 209px;
+  margin-bottom: 18px;
+
+`;
+export const OthersFrameworksText = styled(Subtitle)`
+  color: ${({ theme }) => theme.colors.white};
+  width: 209px;
+  margin-bottom: 18px;
+
+`;
+export const UnorderedListTecnology = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style-type: disc;
+  list-style-position: inside;
+`;
+
+export const ListTecnologys = styled(Paragraph1).attrs({ as: 'li' })`
+  color: ${({ theme }) => theme.colors.white};
+  padding: 2px 0;
+  width: auto; 
 `;
--- src/components/TechnologyBanner/technologybanner.tsx
+++ src/components/TechnologyBanner/technologybanner.tsx
@@ -1,13 +1,23 @@
 'use client';
 import {
   ContainerMainText,
-  ContainerSubText,
   ContainerTechnology,
   LearnAboutText,
   TechnologyContainer,
   TextRow,
   MoreTechnologiesText,
+  CardTechnology,
+  TitleCardTechnology,
+  MainTitleTechnology,
+  TechnologyDescription,
+  OthersFrameworksText,
+  UnorderedListTecnology,
+  ListTecnologys,
 } from './styles';
+import Image from 'next/image';
+import reactLogo from '../../../public/react-logo.svg';
+import cSharpLogo from '../../../public/c-logo.svg';
+import azureLogo from '../../../public/azure-logo.svg';
 
 export const TechnologyBanner = () => {
   return (
@@ -22,11 +32,70 @@ export const TechnologyBanner = () => {
           <LearnAboutText color='white'>Le code</LearnAboutText>
           <LearnAboutText color='green'>domina</LearnAboutText>
         </TextRow>
-          <MoreTechnologiesText>As tecnologias mais atuais em prol da efiência e performance</MoreTechnologiesText>
+        <MoreTechnologiesText>As tecnologias mais atuais em prol da efiência e performance</MoreTechnologiesText>
       </ContainerMainText>
-
       <ContainerTechnology>
-        <p>a</p>
+        <CardTechnology>
+          <TitleCardTechnology>Front-end</TitleCardTechnology>
+          <Image
+            alt='React logo'
+            src={reactLogo}
+          />
+          <MainTitleTechnology>React</MainTitleTechnology>
+          <TechnologyDescription>
+            Utilizamos este e outros frameworks conhecidos como os melhores do mercado
+          </TechnologyDescription>
+          <OthersFrameworksText>Outros frameworks que utilizamos:</OthersFrameworksText>
+          <UnorderedListTecnology>
+            <ListTecnologys>React Native</ListTecnologys>
+            <ListTecnologys>Javascript</ListTecnologys>
+            <ListTecnologys>SASS/CSS3</ListTecnologys>
+            <ListTecnologys>Typescript</ListTecnologys>
+            <ListTecnologys>Visual Code</ListTecnologys>
+          </UnorderedListTecnology>
+        </CardTechnology>
+
+        <CardTechnology>
+          <TitleCardTechnology>Back-end</TitleCardTechnology>
+          <Image
+            alt='C# logo'
+            src={cSharpLogo}
+          />
+          <MainTitleTechnology>React</MainTitleTechnology>
+          <TechnologyDescription>
+          Desenvolvemos uma estrutura robusta, com integrações e APIs para otimizar o desempenho.
+          </TechnologyDescription>
+          <OthersFrameworksText>Outros frameworks que utilizamos:</OthersFrameworksText>
+          <UnorderedListTecnology>
+            <ListTecnologys>PHP</ListTecnologys>
+            <ListTecnologys>Python</ListTecnologys>
+            <ListTecnologys>Ruby</ListTecnologys>
+            <ListTecnologys>Node.js</ListTecnologys>
+            <ListTecnologys>Visual Basic</ListTecnologys>
+          </UnorderedListTecnology>
+        </CardTechnology>
+
+        
+        <CardTechnology>
+          <TitleCardTechnology>Infraestrutura</TitleCardTechnology>
+          <Image
+            alt='Azure logo'
+            src={azureLogo}
+          />
+          <MainTitleTechnology>Azure</MainTitleTechnology>
+          <TechnologyDescription>
+          Gerenciamos a infraestrutura dos projetos com o que há de melhor no mercado.
+          </TechnologyDescription>
+          <OthersFrameworksText>Outros frameworks que utilizamos:</OthersFrameworksText>
+          <UnorderedListTecnology>
+            <ListTecnologys>SQL Server</ListTecnologys>
+            <ListTecnologys>AWS</ListTecnologys>
+            <ListTecnologys>Github</ListTecnologys>
+            <ListTecnologys>ConsumosDB</ListTecnologys>
+            <ListTecnologys>Cloudnary</ListTecnologys>
+            <ListTecnologys>Auth0</ListTecnologys>
+          </UnorderedListTecnology>
+        </CardTechnology>
       </ContainerTechnology>
     </TechnologyContainer>
   );
