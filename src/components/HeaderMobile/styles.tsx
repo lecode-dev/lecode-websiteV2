81d1cb0afd62a4dc2d36bfdf16bee9cab7c7b8fd
KaiqueNoronha <kaiquesic@hotmail.com>
2024-07-01 15:06:52 -0300

    Versão Final

--- src/components/HeaderMobile/styles.tsx
+++ src/components/HeaderMobile/styles.tsx
@@ -1,37 +1,43 @@
 import styled from 'styled-components';
+import Image from 'next/image';
+import { Styles } from '@/styles';
+import { ContactButton } from '../ContactButton';
 
-export const HeaderContainer = styled.header<{ scrolled: boolean }>`
-  display: none;
-  background-color: ${(props) => (props.scrolled ? 'white' : 'transparent')};
-  box-shadow: ${(props) => (props.scrolled ? '0 2px 4px rgba(0, 0, 0, 0.1)' : 'none')};
-  transition: background-color 0.3s ease, box-shadow 0.3s ease;
+export const NavbarContainer = styled.header`
   position: fixed;
-  width: 100%;
   top: 0;
   left: 0;
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
   z-index: 1000;
-  
-  @media (max-width: 768px) {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px 20px;
+  padding: 0.625rem; 
+`;
+
+export const NavbarContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%; 
+
+  @media (min-width: 1440px) {
+    width: 90rem; 
   }
 `;
 
-export const Logo = styled.img<{ scrolled: boolean }>`
-  height: 40px;
-  transition: opacity 0.3s ease;
-  opacity: ${(props) => (props.scrolled ? 0 : 1)};
-`;
+export const LecodeLogo = styled(Image)`
+  margin-right: auto; 
 
-export const NavLinks = styled.nav<{ scrolled: boolean }>`
-  display: ${(props) => (props.scrolled ? 'flex' : 'none')};
-  gap: 15px;
-
-  a {
-    text-decoration: none;
-    color: black;
-    font-size: 16px;
+  @media (min-width: 1440px) {
+    margin-right: 10%; 
   }
 `;
+
+export const StyledContactButton = styled(ContactButton) ` 
+
+`; 
